635da01976e965d02fd8254cd0d9e8d3e2a24933
Spencer <spencerobsitnik@gmail.com>
2024-04-11 10:12:51 -0400

    interaction caster

---
+++ app/api/[[...routes]]/route.tsx
@@ -0,0 +1,168 @@
+/** @jsxImportSource frog/jsx */
+
+import { Button, Frog, TextInput } from 'frog'
+import { devtools } from 'frog/dev'
+// import { neynar } from 'frog/hubs'
+import { pinata } from 'frog/hubs'
+import { handle } from 'frog/next'
+import { serveStatic } from 'frog/serve-static'
+import { NeynarAPIClient } from "@neynar/nodejs-sdk";
+
+const client = new NeynarAPIClient(process.env.NEYNAR_API_KEY!);
+
+const app = new Frog({
+  assetsPath: '/',
+  basePath: '/api',
+  // hub: neynar({ apiKey: 'NEYNAR_FROG_FM' })
+  hub: pinata()
+})
+
+app.frame('/', (c) => {
+  return c.res({
+    image: (
+      <div
+        style={{
+          alignItems: 'center',
+          background: 'linear-gradient(to right, #432889, #17101F)',
+          backgroundSize: '100% 100%',
+          display: 'flex',
+          flexDirection: 'column',
+          flexWrap: 'nowrap',
+          height: '100%',
+          justifyContent: 'center',
+          textAlign: 'center',
+          width: '100%',
+        }}
+      >
+        <div
+          style={{
+            color: 'white',
+            fontSize: 60,
+            fontStyle: 'normal',
+            letterSpacing: '-0.025em',
+            lineHeight: 1.4,
+            marginTop: 30,
+            padding: '0 120px',
+            whiteSpace: 'pre-wrap',
+          }}
+        > Has @ciniz interacted with your post?
+        </div>
+      </div>
+    ),
+    intents: [
+        <Button value="ciniz">Check now</Button>,
+    ],
+    action: '/response'
+  })
+})
+
+const cinizFID = 7251
+
+app.frame('/response', async (c) => {
+  const { verified, frameData, buttonValue } = c
+
+  if (!verified) {
+    return ReturnUnverified(c, "Please login to Farcaster")
+  }
+
+  const senderFid = frameData?.fid
+
+  if (!senderFid) {
+    return ReturnUnverified(c, "Please login to farcaster")
+  }
+
+  console.log(buttonValue)
+
+  let casts = await client.fetchAllCastsCreatedByUser(senderFid, {
+    limit: 100
+  })
+
+  let reacted = false
+  for (let c of casts.result.casts) {
+    const reactionFids = c.reactions.fids;
+    if (reactionFids.includes(cinizFID)) {
+      reacted = true
+    }
+  }
+
+
+  let message = "🥺 Nope. Try bothering them more. 🥺"
+  if (reacted) {
+    message = "🥳 Yup!  You must be special! 🥳"
+  }
+
+  return c.res({
+    image: (
+      <div
+        style={{
+          alignItems: 'center',
+          background:'linear-gradient(to right, #432889, #17101F)',
+          backgroundSize: '100% 100%',
+          display: 'flex',
+          flexDirection: 'column',
+          flexWrap: 'nowrap',
+          height: '100%',
+          justifyContent: 'center',
+          textAlign: 'center',
+          width: '100%',
+        }}
+      >
+        <div
+          style={{
+            color: 'white',
+            fontSize: 60,
+            fontStyle: 'normal',
+            letterSpacing: '-0.025em',
+            lineHeight: 1.4,
+            marginTop: 30,
+            padding: '0 120px',
+            whiteSpace: 'pre-wrap',
+          }}
+        >
+          {`${message}`}
+        </div>
+      </div>
+    ),
+  })
+})
+
+
+function ReturnUnverified(c: any, message: string) {
+  return c.res({
+    image: (
+      <div
+        style={{
+          alignItems: 'center',
+          background: 'black',
+          backgroundSize: '100% 100%',
+          display: 'flex',
+          flexDirection: 'column',
+          flexWrap: 'nowrap',
+          height: '100%',
+          justifyContent: 'center',
+          textAlign: 'center',
+          width: '100%',
+        }}
+      >
+        <div
+          style={{
+            color: 'white',
+            fontSize: 60,
+            fontStyle: 'normal',
+            letterSpacing: '-0.025em',
+            lineHeight: 1.4,
+            marginTop: 30,
+            padding: '0 120px',
+            whiteSpace: 'pre-wrap',
+          }}
+        > `{message}`
+        </div>
+      </div>
+    ),
+  })
+}
+
+devtools(app, { serveStatic })
+
+export const GET = handle(app)
+export const POST = handle(app)
